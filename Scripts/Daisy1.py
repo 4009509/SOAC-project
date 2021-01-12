6a475bde5b464b60e4341d516a6da81cae7d95c0
Timothy van der Duim <36629004+timotheus@users.noreply.github.com>
2021-01-12 11:20:48 +0100

    fresh daisies

--- Scripts/Daisy1.py
+++ Scripts/Daisy1.py
@@ -29,9 +29,9 @@ plt.rc('figure', figsize = (12, 5))
 '''
 
 S_0 = 1366 # solar constant, W/m^2
-alpha_g = 0.25 # albedo ground
+alpha_g = 0.5 # albedo ground
 alpha_w = 0.75 # albbedo white daisies
-alpha_b = 0.15 # albedo black daisies
+alpha_b = 0.25 # albedo black daisies
 gamma = 0.3 # death rate daisies per unit time
 p = 1 # proportion of the planets area which is fertile ground
 beta = 16 # what is this? (W m-2 K-1)
@@ -68,7 +68,7 @@ def T_daisy(L, A_w, A_b, daisy_type):
     
 def growth_rate(L, A_w, A_b, daisy_type):
     Tdaisy = T_daisy(L, A_w, A_b, daisy_type)
-    return 1 - 4 * (T_opt - Tdaisy)**2 / (T_max - T_min)**2
+    return max(1 - 4 * (T_opt - Tdaisy)**2 / (T_max - T_min)**2, 0)
 
 def dA_dt(L, A_w, A_b, daisy_type):
     A_g = p - A_w  - A_b
@@ -87,13 +87,14 @@ def dA_dt(L, A_w, A_b, daisy_type):
 '''
 
 t_init = 0 # initial time
-t_end = 1e2 # end time of simulation in seconds
-dt = 0.1 # time step in seconds
+t_end = 1e1 # end time of simulation in seconds
+dt = 0.01 # time step in seconds
 time = np.arange(t_init, t_end + dt, dt) # time array
 
-lums = np.concatenate([np.arange(0.6, 2, 0.05), np.arange(1.95, 0.55, -0.05)])
+lums = np.concatenate([np.arange(0.6, 2.4, 0.05), np.arange(2.35, 0.55, -0.05)])
 temps = []
 aws = []
+abss = []
 
 for L in lums:
     print(L)
@@ -105,25 +106,24 @@ for L in lums:
     
     # initial conditions
     idx = 0
-    if L == lums[0]:
-        A_w[idx] = 0.5 # start with half of the available area white daisies
-        A_b[idx] = 0.5 # start with half of the available area black daisies
-    else:
-        if A_w_max < 0.01 and A_b_max < 0.01:
-            A_w[idx] = 0.01
-            A_b[idx] = 0.01
-        elif A_w_max < 0.01 and A_b_max >= 0.01:
-            A_w[idx] = 0.01
-            A_b[idx] = A_b_max
-        elif A_w_max >= 0.01 and A_b_max < 0.01:
-            A_w[idx] = A_w_max
-            A_b[idx] = 0.01
-        else:
-            A_w[idx] = A_w_max # start with steady state soln previous iteration white daisies
-            A_b[idx] = A_b_max # start with steady state soln previous iteration black daisies
+    #if L == lums[0]:
+    A_w[idx] = 0.5 # start with half of the available area white daisies
+    A_b[idx] = 0.5 # start with half of the available area black daisies
+    # else:
+    #     if A_w_max < 0.01 and A_b_max < 0.01:
+    #         A_w[idx] = 0.01
+    #         A_b[idx] = 0.01
+    #     elif A_w_max < 0.01 and A_b_max >= 0.01:
+    #         A_w[idx] = 0.01
+    #         A_b[idx] = A_b_max
+    #     elif A_w_max >= 0.01 and A_b_max < 0.01:
+    #         A_w[idx] = A_w_max
+    #         A_b[idx] = 0.01
+    #     else:
+    #         A_w[idx] = A_w_max # start with steady state soln previous iteration white daisies
+    #         A_b[idx] = A_b_max # start with steady state soln previous iteration black daisies
     # print(A_w[idx], A_b[idx])
     for idx in range(len(time) - 1):
-        
         X_0 = A_w[idx]
         Y_0 = 0#A_b[idx]
         X_1 = X_0 + dA_dt(L, X_0, Y_0, daisy_type = "white") * dt / 2
@@ -140,10 +140,12 @@ for L in lums:
     A_w_max = A_w[-1]
     A_b_max = A_b[-1]
     aws.append(A_w_max)
+    abss.append(A_b_max)
     temps.append(avg_T_g(L, A_w[-1], A_b[-1]))
     
 plt.figure()
 plt.plot(lums,temps)
+#plt.plot(lums,aws)
 plt.xlabel("Solar luminosity")
 plt.ylabel("Temp (deg C)")
 plt.grid()
